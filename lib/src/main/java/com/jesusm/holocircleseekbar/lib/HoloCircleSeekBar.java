34513af3b5645ea9dcfb752b0ba589874746d84c
peter <peter.detzner@gmail.com>
2016-06-01 13:26:13 +0200

    Update HoloCircleSeekBar.java

--- lib/src/main/java/com/jesusm/holocircleseekbar/lib/HoloCircleSeekBar.java
+++ lib/src/main/java/com/jesusm/holocircleseekbar/lib/HoloCircleSeekBar.java
@@ -445,8 +445,8 @@ public class HoloCircleSeekBar extends View {
 	
     public void setValue(float newValue) {
     	int _newVal = (int)(newValue);
-    	if(_newVal == 0 ){
-    	    float newAngle = (float) (360.0 * (max / max));
+    	if(_newVal == 0 || _newVal == this.max ){
+    	    float newAngle = (float) (360.0 * (_newVal / this.max));
             arc_finish_radians = (int)calculateAngleFromRadians(calculateRadiansFromAngle(newAngle)) ;
 	    mAngle = calculateAngleFromRadians(arc_finish_radians);
 	    setText(String.valueOf(getNormedValue(_newVal)));
@@ -457,7 +457,8 @@ public class HoloCircleSeekBar extends View {
             float newAngle = (float) (360.0 * (newValue / max));
             arc_finish_radians = (int) calculateAngleFromRadians(calculateRadiansFromAngle(newAngle)) + 1;
 		mAngle = calculateAngleFromRadians(arc_finish_radians);
-		setText(String.valueOf(calculateTextFromAngle(arc_finish_radians)));
+	//	setText(String.valueOf(calculateTextFromAngle(arc_finish_radians)));
+	 	setText(String.valueOf(getNormedValue(_newVal)));
 		updatePointerPosition();
 		invalidate();
         }
